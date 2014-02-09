8e9c9b385c42366c5ca391cc6037ab20bf31db89
Ben West <bewest@gmail.com>
2014-02-08 19:10:39 -0800

    expose library via index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var api = {
+    xls: require('./lib/xls')
+  , download: require('./lib/download')
+  , render: require('./lib/render')
+  , parsers: require('./lib/parsers')
+  , utils: require('./lib/utils')
+};
+
+module.exports = api;
